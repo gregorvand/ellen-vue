17c11e3ff6866ad45c48962f6487fc6503701172
Gregor Vand <gregor@pixelcabin.io>
2021-08-09 14:30:29 +0800

    enable importing scss modules globally

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,11 @@ module.exports = {
   devServer: {
     disableHostCheck: true,
   },
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `@import "~@/styles/lib/foundation.scss";`,
+      },
+    },
+  },
 }
