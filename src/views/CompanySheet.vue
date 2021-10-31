04a6fbcbae6bf5d5e8c3d86b9a81ee593e7a2682
Gregor <gregor@vand.hk>
2021-10-31 21:46:59 +0800

    Updates Chart functionality for newer data/updated endpoints. Removes legacy request no longer used (#62)

--- src/views/CompanySheet.vue
+++ src/views/CompanySheet.vue
@@ -3,27 +3,18 @@
     <div class="container row">
       <div class="company-sheet-name">
         <h1 v-if="company.nameIdentifier">{{ company.nameIdentifier }}</h1>
+        <h4 v-if="company.emailIdentifier">
+          {{ companyDomain }}
+        </h4>
         <h1 v-else><BaseLoadingSpinner /></h1>
       </div>
+
       <span v-if="company.ticker">Symbol: {{ company.ticker }}</span>
       <div>
         <span class="key-line"></span>
         <!-- v if company is Edison -->
         <span>Avg orders/day</span>
       </div>
-    </div>
-    <template v-if="publicData && company.ticker">
-      <div>
-        <ul>
-          <li>{{ publicData.symbol }}</li>
-          <li>{{ publicData.date }}</li>
-          <li>{{ publicData.period }}</li>
-          <li>ROA: {{ publicData.returnOnAssets }}</li>
-        </ul>
-      </div>
-    </template>
-    <div v-else>
-      <span v-if="company.ticker">Getting public data...</span>
     </div>
     <LineChart
       v-if="!company.ticker"
@@ -34,7 +25,6 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
 import LineChart from '@/components/ChartV2'
 
 export default {
@@ -46,10 +36,15 @@ export default {
   beforeCreate() {
     this.$store.dispatch('company/fetchCompany', this.$route.params.id)
   },
-  computed: mapState({
-    company: (state) => state.company.currentCompany,
-    publicData: (state) => state?.company?.publicData[0],
-  }),
+  computed: {
+    company() {
+      return this.$store.state.company.currentCompany
+    },
+    companyDomain() {
+      let emailSplit = this.company.emailIdentifier.split('@')
+      return emailSplit[1]
+    },
+  },
 }
 </script>
 
@@ -61,6 +56,8 @@ export default {
 
   &-name {
     flex-direction: row;
+    justify-content: flex-start;
+    text-align: left;
   }
 }
 </style>
