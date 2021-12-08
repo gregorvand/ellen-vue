33dcd1a9f6db747b9a09318ebec0e9bb3b226431
Gregor Vand <gregor@pixelcabin.io>
2021-12-08 15:17:19 +0800

    Company can be followed from its own page

--- src/views/CompanySheet.vue
+++ src/views/CompanySheet.vue
@@ -7,12 +7,16 @@
         </h1>
         <h4 v-if="companyObject.data !== undefined">
           {{ companyDomain }} <br />
+          {{ companyObject.data.emailIdentifier }}
         </h4>
         <h1 v-else><BaseLoadingSpinner /></h1>
       </div>
       <div>
         <span class="key-line"></span>
         <!-- v if company is Edison -->
+        <button v-if="!isFollowed" @click="followUnfollow" ref="followButton">
+          Follow
+        </button>
         <span>Avg orders/month</span>
       </div>
     </div>
@@ -26,6 +30,8 @@
 <script>
 import LineChart from '@/components/ChartV3'
 import ChartDataService from '../services/ChartDataService'
+import { mapState } from 'vuex'
+import axios from 'axios'
 // import { computed } from 'vue-demi'
 
 export default {
@@ -47,6 +53,45 @@ export default {
     companyDomain() {
       let emailSplit = this.companyObject?.data.emailIdentifier.split('@')
       return emailSplit[1]
+    },
+    company() {
+      return {
+        companyEmail: this.companyObject.emailIdentifier,
+        companyName: this.companyObject.nameIdentifier,
+        id: this.companyObject.id,
+      }
+    },
+    isFollowed() {
+      return this.followedCompanies.filter(
+        (company) =>
+          company.id.toString() == this.companyObject?.data?.id.toString()
+      ).length > 0
+        ? true
+        : false
+    },
+    ...mapState('followedCompanies', ['followedCompanies']),
+  },
+  methods: {
+    followUnfollow() {
+      const companyObject = this.companyObject.data
+
+      axios({
+        method: 'post',
+        url: `${process.env.VUE_APP_API_URL}/api/users/update/companies`,
+        data: {
+          selectedCompanies: [
+            {
+              companyEmail: companyObject.emailIdentifier,
+              companyIndustry: companyObject.industry,
+              companyName: companyObject.nameIdentifier,
+              id: companyObject.id,
+            },
+          ],
+        },
+      }).then((data) => {
+        console.log(data)
+        this.$refs.followButton.innerText = 'Followed!'
+      })
     },
   },
 }
