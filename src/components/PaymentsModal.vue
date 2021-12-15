746d8860655d84a4cd42c124ef2c3551b83a971b
Gregor Vand <gregor@pixelcabin.io>
2021-12-15 16:04:39 +0800

    Basic PaymentsModal structure

---
+++ src/components/PaymentsModal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button @click="toggleMode('subscription')">Subscribe</button>
+    <button @click="toggleMode('single')">Single</button>
+    <div v-if="mode == 'single'">
+      <Payments />
+    </div>
+    <div v-if="mode == 'subscription'">
+      <PaymentsSubscription />
+    </div>
+  </div>
+</template>
+
+<script>
+import Payments from '@/components/Payments'
+import PaymentsSubscription from '@/components/PaymentsSubscription'
+export default {
+  components: {
+    PaymentsSubscription,
+    Payments,
+  },
+  data: function () {
+    return {
+      mode: 'subscription',
+    }
+  },
+  methods: {
+    toggleMode(mode) {
+      this.mode = mode
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
