2b9d0d880272a9f101dc504d60c7f9b4aa1aa63b
Gregor <gregor@vand.hk>
2021-12-15 14:19:37 +0800

    Updates transaction UI (#91)

--- src/components/TransactionRecord.vue
+++ src/components/TransactionRecord.vue
@@ -1,23 +1,31 @@
 <template>
-  <div class="transaction-wrapper">
+  <div class="container">
     <h3>Credit Transaction History</h3>
-    <div class="grid-table headers">
-      <span>Amount</span>
-      <span>Info</span>
-      <span>Company</span>
-      <span>Transaction Date</span>
-    </div>
-    <div
-      class="grid-table"
-      v-for="transaction in transactionList"
-      :key="transaction.id"
-    >
-      <span>{{ transaction.value | plusMinus }}</span>
-      <span>{{ transaction.method }}</span>
-      <span v-if="transaction.emailIdentifier !== 'undefined'">{{
-        transaction.emailIdentifier | filterEmail
-      }}</span>
-      <span>{{ transaction.createdAt | formatDate }}</span>
+    <h4>(Latest 20)</h4>
+    <div class="transaction-wrapper">
+      <div class="grid-table headers">
+        <span>Date</span>
+        <span class="not-tablet not-mobile">Info</span>
+        <span>Company</span>
+        <span>Amount</span>
+      </div>
+      <div class="overflow-y">
+        <div
+          class="grid-table"
+          v-for="transaction in transactionList"
+          :key="transaction.id"
+        >
+          <span>{{ transaction.createdAt | formatDate }}</span>
+          <span class="not-tablet not-mobile">{{ transaction.method }}</span>
+          <span v-if="transaction.emailIdentifier !== 'undefined'">{{
+            transaction.emailIdentifier | filterEmail
+          }}</span>
+
+          <span :class="{ bolder: transaction.value > 0 }">{{
+            transaction.value | plusMinus
+          }}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -42,7 +50,7 @@ export default {
     plusMinus: function (value) {
       parseInt(value)
       if (value > 0) {
-        return `+${value}`
+        return `+ ${value}`
       } else {
         return `${value}`
       }
@@ -56,7 +64,7 @@ export default {
       }
     },
     formatDate(date) {
-      let readableDate = dayjs(date).format('MMMM DD, YYYY')
+      let readableDate = dayjs(date).format('DD-MMM-YY')
       return readableDate
     },
   },
@@ -64,26 +72,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+h3 {
+  margin: 10px auto 0;
+}
+h4 {
+  margin: 0 auto;
+}
 .transaction-wrapper {
   margin: 20px auto;
   width: 100%;
-  max-width: 900px;
+  max-width: 800px;
   background-color: $color-ellen-light-gray;
   padding: 10px;
-
-  h3 {
-    margin-bottom: 10px;
-  }
+  border-radius: $border-radius;
 }
+
+.overflow-y {
+  max-height: 200px;
+  padding: 5px;
+}
+
 .grid-table {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-columns: 0.5fr 1.5fr 1fr;
   width: 100%;
+  padding-right: 10px;
+
+  @include breakpoint(medium up) {
+    grid-template-columns: 0.5fr 2fr 1fr;
+  }
+
+  @include breakpoint(large up) {
+    grid-template-columns: 0.5fr 1fr 2fr 1fr;
+  }
 
   span {
-    max-width: 250px;
-    text-align: left;
+    text-align: right;
     border-bottom: solid #efeffe thin;
+    font-size: rem-calc(14px);
   }
 
   &.headers {
