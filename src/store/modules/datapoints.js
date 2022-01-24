767a8502c83215d6caec10ab160b289f39bae817
Gregor Vand <gregor@pixelcabin.io>
2022-01-24 22:27:19 +0800

    Add pulling and rendering of ACT value

--- src/store/modules/datapoints.js
+++ src/store/modules/datapoints.js
@@ -4,17 +4,21 @@ export const namespaced = true
 
 export const state = () => ({
   aov: 'loading',
+  act: 'loading',
 })
 
 export const getters = {
   currentPeriodAov: (state) => {
-    return state.aov
+    return state.act
   },
 }
 
 export const mutations = {
   SET_AOV(state, aov) {
     state.aov = aov
+  },
+  SET_ACT(state, act) {
+    state.act = act
   },
 }
 
@@ -28,7 +32,6 @@ export const actions = {
       },
     })
       .then((aov) => {
-        let aovResult
         if (aov.data == '') {
           commit('SET_AOV', 'coming soon')
         } else {
@@ -39,7 +42,26 @@ export const actions = {
         commit('SET_AOV', { message: 'Puchase data to unlock AOV' })
       })
   },
-  setBalance({ commit }, aovResult) {
-    commit('SET_AOV', aovResult)
+  fetchAct({ commit }, payload) {
+    console.log(payload)
+    axios({
+      method: 'post',
+      url: `${process.env.VUE_APP_API_URL}/api/act`,
+      data: {
+        from_domain: payload,
+      },
+    })
+      .then((act) => {
+        if (act.data == '') {
+          commit('SET_ACT', 'coming soon')
+        } else {
+          commit('SET_ACT', act.data)
+        }
+      })
+      .catch((err) => {
+        commit('SET_ACT', {
+          message: 'Puchase data to unlock Avg. Customer Orders',
+        })
+      })
   },
 }
