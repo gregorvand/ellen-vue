b0e9c34a1ebbe83d7162b262cb4ecdac00412992
Gregor Vand <gregor@pixelcabin.io>
2022-02-18 15:46:15 +0800

    Adds marketshare table with static data

--- src/components/MarketShare.vue
+++ src/components/MarketShare.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="marketshare">
-    <h3>Market share demo</h3>
+    <!-- <h3>Market share demo</h3> -->
 
     <div class="chart-frame">
       <div class="watermark">
@@ -23,8 +23,59 @@
         :options="options"
         :class="'ellen-chart'"
         :width="800"
+        :height="300"
       />
     </div>
+
+    <table>
+      <caption>
+        <div class="company-list">
+          <div scope="col">Magic Spoon</div>
+          <div scope="col">Catalina Crunch</div>
+          <div scope="col">Schoolyard Snacks</div>
+        </div>
+      </caption>
+      <thead>
+        <tr>
+          <th scope="col" class="text-left">Month</th>
+          <th scope="col">Revenue</th>
+          <th scope="col">Market share</th>
+          <th scope="col">Revenue</th>
+          <th scope="col">Market share</th>
+          <th scope="col">Revenue</th>
+          <th scope="col">Market share</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td data-label="Period" class="text-left">Oct Est</td>
+          <td data-label="Magic Spoon">$3,461,531.70</td>
+          <td>79.51%</td>
+          <td data-label="Catalina Crunch">$444,120.36</td>
+          <td>10.20%</td>
+          <td data-label="Schoolyard Snacks">$447,927.00</td>
+          <td>10.29%</td>
+        </tr>
+        <tr>
+          <td scope="row" data-label="Period" class="text-left">Nov Est</td>
+          <td data-label="Magic Spoon">$3,426,473.16</td>
+          <td>80.34%</td>
+          <td data-label="Catalina Crunch">$444,201.14</td>
+          <td>10.41%</td>
+          <td data-label="Schoolyard Snacks">$394,402.22</td>
+          <td>9.25%</td>
+        </tr>
+        <tr>
+          <td scope="row" data-label="Period" class="text-left">Dec Est</td>
+          <td data-label="Magic Spoon">$2,400,591.60</td>
+          <td>77.30%</td>
+          <td data-label="Catalina Crunch">$333,175.93</td>
+          <td>10.73%</td>
+          <td data-label="Schoolyard Snacks">$371,956.47</td>
+          <td>11.98%</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -87,11 +138,6 @@ export default {
         ],
       },
       options: {
-        plugins: {
-          legend: {
-            reverse: true,
-          },
-        },
         interaction: {
           mode: 'nearest',
           interesectors: true,
@@ -112,6 +158,12 @@ export default {
             scaleLabel: {
               display: true,
               labelString: 'Percentage',
+            },
+          },
+          x: {
+            ticks: {
+              maxRotation: 45,
+              minRotation: 45,
             },
           },
         },
@@ -150,6 +202,108 @@ export default {
     p {
       font-size: 12px;
     }
+  }
+}
+
+table {
+  border: 1px solid #ccc;
+  border-collapse: collapse;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  table-layout: fixed;
+}
+
+table caption {
+  font-size: 1.5em;
+  margin: 0.5em 0 0.75em;
+}
+
+table tr {
+  background-color: #f8f8f8;
+  border: 1px solid #ddd;
+  padding: 0.35em;
+}
+
+table th,
+table td {
+  padding: 0.625em;
+  text-align: center;
+  font-size: 13px;
+}
+
+table th {
+  font-size: 0.85em;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+}
+
+.company-list {
+  display: flex;
+  width: 100%;
+  padding-left: 15%;
+
+  div {
+    flex-basis: 33%;
+    font-size: 18px;
+  }
+
+  @include breakpoint(small only) {
+    display: none;
+  }
+}
+
+.text-left {
+  @include breakpoint(medium up) {
+    text-align: left;
+  }
+}
+
+@include breakpoint(small only) {
+  table {
+    border: 0;
+  }
+
+  table caption {
+    font-size: 1.3em;
+  }
+
+  table thead {
+    border: none;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+  }
+
+  table tr {
+    border-bottom: 3px solid #ddd;
+    display: block;
+    margin-bottom: 0.625em;
+  }
+
+  table td {
+    border-bottom: 1px solid #ddd;
+    display: block;
+    text-align: right;
+  }
+
+  table td::before {
+    /*
+    * aria-label has no advantage, it won't be read inside a table
+    content: attr(aria-label);
+    */
+    content: attr(data-label);
+    float: left;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+
+  table td:last-child {
+    border-bottom: 0;
   }
 }
 </style>
