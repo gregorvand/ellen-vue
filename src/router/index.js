e64fad0435efc5fb90b416149c574380080de4bc
Gregor Vand <gregor@pixelcabin.io>
2022-02-25 14:41:20 +0800

    Adds GTM package / ID

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueGtm from '@gtm-support/vue2-gtm'
 import EventCreate from '../views/EventCreate.vue'
 import EventList from '../views/EventList.vue'
 import EventShow from '../views/EventShow.vue'
@@ -20,6 +21,23 @@ import Vue2TouchEvents from 'vue2-touch-events'
 
 Vue.use(VueRouter)
 Vue.use(Vue2TouchEvents)
+
+Vue.use(VueGtm, {
+  id: 'GTM-N25JZLT', // Your GTM single container ID, array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy'] or array of objects [{id: 'GTM-xxxxxx', queryParams: { gtm_auth: 'abc123', gtm_preview: 'env-4', gtm_cookies_win: 'x'}}, {id: 'GTM-yyyyyy', queryParams: {gtm_auth: 'abc234', gtm_preview: 'env-5', gtm_cookies_win: 'x'}}], // Your GTM single container ID or array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy']
+  queryParams: {
+    // Add URL query string when loading gtm.js with GTM ID (required when using custom environments)
+    // gtm_auth: 'AB7cDEf3GHIjkl-MnOP8qr',
+    // gtm_preview: 'env-4',
+    // gtm_cookies_win: 'x',
+  },
+  defer: false, // Script can be set to `defer` to speed up page load at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible). Defaults to false, so the script is loaded `async` by default
+  enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
+  debug: true, // Whether or not display console logs debugs (optional)
+  loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
+  vueRouter: VueRouter, // Pass the router instance to automatically sync with router (optional)
+  ignoredViews: ['homepage'], // Don't trigger events for specified router names (optional)
+  trackOnNextTick: false, // Whether or not call trackView in Vue.nextTick
+})
 
 // Stripe set up
 import { StripePlugin } from '@vue-stripe/vue-stripe'
